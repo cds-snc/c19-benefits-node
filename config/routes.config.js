50966d1fa744f797b67acafb2a317bfeb755a63c
Kate Drwecka <katedrwecka@gmail.com>
2020-03-27 12:26:49 -0400

    add pages 1-5 from figma file currently no routing logic, just the pages exist removed the h1, but PR #1 brings it back into the radios macro

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,6 +1,10 @@
 // 1) add your route(s) here ⬇️
 const routes = [
   { name: 'start', path: { en: '/start', fr: '/debut' } },
+  { name: 'question-lost-job-c19', path: { en: '/lost-job-c19',  fr: '/lost-job-c19'}},
+  { name: 'question-why-stop-working', path: { en: '/why-stop-working',  fr: '/why-stop-working'}},
+  { name: 'question-your-situation', path: { en: '/your-situation',  fr: '/your-situation'}},
+  { name: 'question-applied-to-EI', path: { en: '/applied-to-EI',  fr: '/applied-to-EI'}},
   { name: 'personal', path: { en: '/personal', fr: '/personnel' } },
   { name: 'confirmation', path: '/confirmation' },
 ]
