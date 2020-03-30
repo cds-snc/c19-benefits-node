da0e2e05781edf11073f554c7325e32bd1d5b57b
Paul Craig <paul_craig_16@hotmail.com>
2020-03-30 14:12:11 -0400

    Use morgan logger with JSON format
    Removed the winston logger entirely in favour of just using morgan,
    which is pretty easy to configure and work with.
    
    Also sound a snippet online showing how to do JSON logs, which are
    easier to search through if we're going to have to be doing that
    in the near future (which I think we will do).
    
    Created a smaller format for dev and a larger format for production,
    which we can see by using 'npm start'.
    
    Winston lets us create lots of different log files and stuff, but
    since we're doing a container deployment, we just want everything
    going to stdout, so we don't have that complex a use case.
    
    Note: not saving any user IP info because that's a can of worms we
    don't want to open anytime soon.
    
    Sources:
    - https://github.com/expressjs/morgan
    - https://gist.github.com/cgmartin/913bb12097ff07132597
    - 7aaf3fbc6ea5d8d6098edd9724a499afbbc940c7

---
+++ config/morgan.config.js
@@ -0,0 +1,41 @@
+var morgan = require('morgan')
+
+morgan.token('version', function getSha() {
+  return process.env.GITHUB_SHA
+})
+
+morgan.token('err', function getErr(req, res) {
+  return res.locals.err
+})
+
+module.exports = (function morganConfig() {
+  return process.env.NODE_ENV === 'production'
+    ? jsonFormatProduction
+    : jsonFormatDev
+})()
+
+/* eslint-disable dot-notation */
+
+function jsonFormatDev(tokens, req, res) {
+  return JSON.stringify({
+    method: tokens['method'](req, res),
+    url: tokens['url'](req, res),
+    status: tokens['status'](req, res),
+    'response-time': tokens['response-time'](req, res) + 'ms',
+  })
+}
+
+function jsonFormatProduction(tokens, req, res) {
+  return JSON.stringify({
+    method: tokens['method'](req, res),
+    url: tokens['url'](req, res),
+    status: tokens['status'](req, res),
+    'response-time': tokens['response-time'](req, res) + 'ms',
+    timestamp: tokens['date'](req, res, 'iso'),
+    'content-length': tokens['res'](req, res, 'content-length'),
+    referrer: tokens['referrer'](req, res),
+    'user-agent': tokens['user-agent'](req, res),
+    err: tokens['err'](req, res),
+    version: tokens['version'](),
+  })
+}
