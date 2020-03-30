301e9051d41dc02db5441355baacaef45380be2d
Dave Samojlenko <dsamojlenko@gmail.com>
2020-03-30 15:03:53 -0400

    update terraform deployments for tf 0.12 and add vars for new deployment

--- terraform/azure/variables.tf
+++ terraform/azure/variables.tf
@@ -14,19 +14,3 @@ variable "docker_image" {
 variable "docker_image_tag" {
   description = "(Optional) Specify the tag to be deployed"
 }
-
-#variable "subscription_id" {
-#  description = "Azure Subscription ID to be used for billing"
-#}
-
-#variable "my_sql_master_password" {
-#  description = "MySql master password"
-#}
-
-#variable "docker_image" {
-#  description = "Docker image name"
-#}
-
-#variable "docker_image_tag" {
-#  description = "Docker image tag"
-#}
