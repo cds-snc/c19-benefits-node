ea94cfd5c1a4cdb81df995bf5f12c1261ccfb1ce
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-03-31 12:23:18 -0400

    Fixing Radio Buttons
    Brought in some changes for Radiobutton macro from the
    CPPD-Medical-Report-Sails app
    
    Localization must occur outside the macro, this will ensure if we do
    string interpolation that the localization files won't have
    post-interpelated strings added.
    
    Added 2 new attributes to the macro options to ensure that safe text
    get's rendered properly.
    
    Renamed some items to make more sense
    
    The macro now takes an array of objects for it's value and text for each
    radio button instead of a plain json object. This fixes a bug where the
    radio buttons were being displayed in an incorrect order.
    
    Removed unused routes and views.

--- routes/question-why-stop-working/schema.js
+++ routes/question-why-stop-working/schema.js
@@ -1,12 +1,6 @@
 /* istanbul ignore file */
 
 const Schema = {
-  firstname: {
-    isLength: {
-      errorMessage: 'errors.firstname.length',
-      options: { min: 3, max: 200 },
-    },
-  },
 }
 
 module.exports = {
