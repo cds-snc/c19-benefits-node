b98c62ca36628bff843f6b46f940760e1fead03f
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-01 12:10:10 -0400

    Added questions 3 to 6 from the April 1 iter doc

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,6 +1,10 @@
 // 1) add your route(s) here ⬇️
 const routes = [
   { name: 'start', path: { en: '/start', fr: '/debut' } },
+  { name: 'question-3', path: { en: '/question-3', fr: '/question-3' } },
+  { name: 'question-4', path: { en: '/question-4', fr: '/question-4' } },
+  { name: 'question-5', path: { en: '/question-5', fr: '/question-5' } },
+  { name: 'question-6', path: { en: '/question-6', fr: '/question-6' } },
 ]
 
 const locales = ['en', 'fr']
