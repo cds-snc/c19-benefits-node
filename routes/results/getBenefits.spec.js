269f9a4e064c81ff69a09b3f3476d4b919941dd3
Dave Samojlenko <dsamojlenko@gmail.com>
2020-04-02 19:46:32 -0400

    add a few tests

--- routes/results/getBenefits.spec.js
+++ routes/results/getBenefits.spec.js
@@ -1,31 +1,57 @@
 const { getBenefits } = require('./getBenefits');
 
 describe('Test the getBenefits calculator', () => {
-  test('It checks ei simple path', () => {
-    const expected = ['ei_regular_cerb', 'mortgage_deferral', 'gst_credit', 'ccb_payment', 'student_loan'];
+  test('It checks ei regular + cerb path', () => {
+    const expected = ['ei_regular_cerb'];
    
     const result = getBenefits({
         'lost_job': '1',
         'no_income': '1',
-        'mortgage_payments': '1',
-        'gst': '1',
-        'ccb': '1',
-        'student_debt': '1',
     });
 
     expect(result).toEqual(expect.arrayContaining(expected))
   })
 
-  test('It checks ei simple path wihtout student loan', () => {
-    const expected = ['ei_regular_cerb', 'mortgage_deferral', 'gst_credit', 'ccb_payment'];
+  test('It checks ei sickness + cerb path', () => {
+    const expected = ['ei_sickness_cerb'];
    
     const result = getBenefits({
         'lost_job': '1',
-        'no_income': '1',
+        'no_income': '4',
+    });
+
+    expect(result).toEqual(expect.arrayContaining(expected))
+  })
+
+  test('It checks cerb-only path', () => {
+    const expected = ['cerb'];
+    const options = ['2', '3', '5', '6'];
+
+    options.forEach(income => {
+      const result = getBenefits({
+        'lost_job': '1',
+        'no_income': income,
+      }); 
+
+      expect(result).toEqual(expect.arrayContaining(expected))
+    })
+  })
+
+  test('It checks the mortgage addon', () => {
+    const expected = ['mortgage_deferral'];
+
+    const result = getBenefits({
         'mortgage_payments': '1',
-        'gst': '1',
-        'ccb': '1',
-        'student_debt': '2',
+    });
+
+    expect(result).toEqual(expect.arrayContaining(expected))
+  })
+
+  test('It checks the rent addon', () => {
+    const expected = ['rent_help'];
+
+    const result = getBenefits({
+        'mortgage_payments': '2',
     });
 
     expect(result).toEqual(expect.arrayContaining(expected))
