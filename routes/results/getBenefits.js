6ba43dea0ef870c08728f56fc8cf7e9f0fdce91e
Dave Samojlenko <dsamojlenko@gmail.com>
2020-04-02 18:35:02 -0400

    split getBenefits out to separate file, add some tests

---
+++ routes/results/getBenefits.js
@@ -0,0 +1,75 @@
+/* istanbul ignore next */
+const getBenefits = (data) => {
+  var results = [];
+
+  // 2a
+  if (data.lost_job === '1') {
+    if (data.no_income === '1') {
+      results.push('ei_regular_cerb')
+    }
+
+    if (data.no_income === '4') {
+      results.push('ei_sickness_cerb')
+    }
+
+    if (['2', '3', '5'].includes(data.no_income)) {
+      results.push('cerb')
+    }
+
+    if (data.no_income ==='6') {
+      results.push('cerb')
+    }
+  }
+
+  // 2b
+  if (data.lost_job === '2') {
+    if (data.some_income === '1') {
+      if (['2', '3'].includes(data.gross_income)) {
+        results.push('ei_workshare')
+      }
+    }
+
+    if (data.some_income === '2') {
+      // retired/rrif
+    }
+
+    if (data.some_income === '3') {
+      // gross_income/days
+    }
+  }
+
+  // 2c
+  if (data.lost_job === '3') {
+    // just the regular flow?
+  }
+
+  if (data.mortgage_payments === '1') {
+    results.push('mortgage_deferral')
+  }
+
+  if (data.mortgage_payments === '2') {
+    results.push('rent_help');
+  }
+
+  if (data.student_debt === '1') {
+    results.push('student_loan')
+  }
+
+  if (['1', '3'].includes(data.gst)) {
+    results.push('gst_credit')
+  }
+
+  if (['1', '3'].includes(data.ccb)) {
+    results.push('ccb_payment')
+  }
+
+  if (data.rrif === '1') {
+    results.push('rrif')
+  }
+
+  return results;
+}
+
+module.exports = {
+    getBenefits,
+}
---
+++ routes/results/getBenefits.spec.js
@@ -0,0 +1,33 @@
+const { getBenefits } = require('./getBenefits');
+
+describe('Test the getBenefits calculator', () => {
+  test('It checks ei simple path', () => {
+    var expected = ['ei_regular_cerb', 'mortgage_deferral', 'gst_credit', 'ccb_payment', 'student_loan'];
+   
+    var result = getBenefits({
+        'lost_job': '1',
+        'no_income': '1',
+        'mortgage_payments': '1',
+        'gst': '1',
+        'ccb': '1',
+        'student_debt': '1',
+    });
+
+    expect(result).toEqual(expect.arrayContaining(expected))
+  })
+
+  test('It checks ei simple path', () => {
+    var expected = ['ei_regular_cerb', 'mortgage_deferral', 'gst_credit', 'ccb_payment'];
+   
+    var result = getBenefits({
+        'lost_job': '1',
+        'no_income': '1',
+        'mortgage_payments': '1',
+        'gst': '1',
+        'ccb': '1',
+        'student_debt': '2',
+    });
+
+    expect(result).toEqual(expect.arrayContaining(expected))
+  })
+})
