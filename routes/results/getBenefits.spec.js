67d320081a93711dffd9d69bb2f8ed68aba96425
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-03 14:27:34 -0400

    Adding more tests for benefits

--- routes/results/getBenefits.spec.js
+++ routes/results/getBenefits.spec.js
@@ -3,7 +3,7 @@ const { getBenefits } = require('./getBenefits');
 describe('Test the getBenefits calculator', () => {
   test('It checks ei regular + cerb path', () => {
     const expected = ['ei_regular_cerb'];
-   
+  
     const result = getBenefits({
         'lost_job': 'lost-all-income',
         'no_income': 'lost-job-employer-closed',
@@ -14,7 +14,7 @@ describe('Test the getBenefits calculator', () => {
 
   test('It checks ei sickness + cerb path', () => {
     const expected = ['ei_sickness_cerb'];
-   
+  
     const result = getBenefits({
         'lost_job': 'lost-all-income',
         'no_income': 'sick-or-quarantined',
@@ -23,9 +23,14 @@ describe('Test the getBenefits calculator', () => {
     expect(result).toEqual(expect.arrayContaining(expected))
   })
 
-  test('It checks cerb-only path', () => {
+  test('It checks cerb-only path 1', () => {
     const expected = ['cerb'];
-    const options = ['self-employed-closed', 'unpaid-leave-to-care', 'parental-recently-cant-return', 'ei-recently-claim-ended'];
+    const options = [
+      'self-employed-closed',
+      'unpaid-leave-to-care',
+      'parental-recently-cant-return',
+      'ei-recently-claim-ended',
+    ];
 
     options.forEach(income => {
       const result = getBenefits({
@@ -35,6 +40,70 @@ describe('Test the getBenefits calculator', () => {
 
       expect(result).toEqual(expect.arrayContaining(expected))
     })
+  })
+
+  test('It checks cerb-only path 2', () => {
+    const expected = ['cerb'];
+
+    const result = getBenefits({
+      'lost_job': 'lost-some-income',
+      'some_income': 'employed-lost-a-job',
+      'gross_income': '5k+',
+      'days_stopped_working': '>14days',
+    }); 
+
+    expect(result).toEqual(expect.arrayContaining(expected))
+  })
+
+  test('It checks cerb-only path 3', () => {
+    const expected = ['cerb'];
+    const options = [
+      '3k-5k',
+      '5k+',
+    ];
+
+    options.forEach(income => {
+      const result = getBenefits({
+        'lost_job': 'lost-some-income',
+        'some_income': 'hours-reduced',
+        'gross_income': income,
+        'days_stopped_working': '>14days',
+      }); 
+
+      expect(result).toEqual(expect.arrayContaining(expected))
+    })
+  })
+
+  test('It checks ei_workshare path ', () => {
+    const expected = ['ei_workshare'];
+
+    const options = [
+      '3k-5k',
+      '5k+',
+    ];
+
+    options.forEach(income => {
+      const result = getBenefits({
+        'lost_job': 'lost-some-income',
+        'some_income': 'hours-reduced',
+        'gross_income': income,
+        'days_stopped_working': '<14days',
+      }); 
+
+      expect(result).toEqual(expect.arrayContaining(expected))
+    })
+  })
+
+  test('It checks the ei_regular addon', () => {
+    const expected = ['ei_regular'];
+
+    const result = getBenefits({
+        'lost_job': 'lost-some-income',
+        'some_income': 'employed-lost-a-job',
+        'gross_income': '3k-5k',
+    });
+
+    expect(result).toEqual(expect.arrayContaining(expected))
   })
 
   test('It checks the mortgage addon', () => {
@@ -56,7 +125,6 @@ describe('Test the getBenefits calculator', () => {
 
     expect(result).toEqual(expect.arrayContaining(expected))
   })
-
   test('It checks the ccb addon', () => {
     const expected = ['ccb_payment'];
     const options = ['yes', 'unsure']
