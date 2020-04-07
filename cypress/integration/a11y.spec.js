1950af108cb41a594bf182ba931865978da4eab9
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-07 14:51:50 -0400

    Adding semi-colons

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -1,12 +1,12 @@
 describe('Accessibility checks', () => {
   beforeEach(() => {
     cy.visit('/')
-  })
+  });
   it('Check to first erro a11y', () => {
     cy.reportA11y()
     cy.get('[data-cy=start]').click()
     cy.reportA11y()
     cy.get('[data-cy=next]').click()
     cy.reportA11y()
-  })
-})
+  });
+});
