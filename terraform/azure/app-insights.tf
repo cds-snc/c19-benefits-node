26298e6d3bfee621db2cd2f39355a212761ad2d9
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-09 14:02:06 -0400

    Scaling up and adding a staging insights

--- terraform/azure/app-insights.tf
+++ terraform/azure/app-insights.tf
@@ -5,3 +5,9 @@ resource "azurerm_application_insights" "covid-benefit" {
   application_type    = "Node.JS"
 }
 
+resource "azurerm_application_insights" "staging" {
+  name                = "${var.name}.app_insight-staging"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  application_type    = "Node.JS"
+}
