3275b8c373a1cecaa632636e51ec0aa3b1ef67c5
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-09 12:42:04 -0400

    fixing tests

--- routes/results/getBenefits.spec.js
+++ routes/results/getBenefits.spec.js
@@ -42,47 +42,25 @@ describe('Test the getBenefits calculator', () => {
       lost_job: 'lost-some-income',
       some_income: 'employed-lost-a-job',
       gross_income: '5k+',
-      days_stopped_working: '>14days',
     })
 
     expect(result).toContain('cerb')
   })
 
   test('It checks cerb-only path 3', () => {
-    const options = ['3k-5k', '5k+']
 
-    options.forEach(income => {
-      const result = getBenefits({
-        lost_job: 'lost-some-income',
-        some_income: 'hours-reduced',
-        gross_income: income,
-        days_stopped_working: '>14days',
-      })
-
-      expect(result).toContain('cerb')
+    const result = getBenefits({
+      gross_income: '5k+',
     })
+
+    expect(result).toContain('cerb')
   })
 
-  test('It checks ei_workshare path ', () => {
-    const options = ['3k-5k', '5k+']
-
-    options.forEach(income => {
-      const result = getBenefits({
-        lost_job: 'lost-some-income',
-        some_income: 'hours-reduced',
-        gross_income: income,
-        days_stopped_working: '<14days',
-      })
-
-      expect(result).toContain('ei_workshare')
-    })
-  })
 
   test('It checks the ei_regular addon', () => {
     const result = getBenefits({
-      lost_job: 'lost-some-income',
-      some_income: 'employed-lost-a-job',
-      gross_income: '3k-5k',
+      lost_job: 'lost-all-income',
+      no_income: 'unsafe-work-conditions',
     })
 
     expect(result).toContain('ei_regular')
@@ -116,7 +94,6 @@ describe('Test the getBenefits calculator', () => {
     })
   })
 
-
   test('It checks the rrif addon', () => {
     const expected = ['rrif']
 
@@ -138,8 +115,6 @@ describe('Test the getBenefits calculator', () => {
   test('It should only pass if it matches everything in the pattern', () => {
     const result = getBenefits({
       lost_job: 'lost-some-income',
-      some_income: 'employed-lost-a-job',
-      gross_income: '5k+',
     })
 
     expect(result).toHaveLength(0)
