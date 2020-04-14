4a5136f94ca673b6a21ee1e86aaa3e0b3f8b3ece
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-04-14 18:00:02 -0400

    add datetime to feedback

--- routes/feedback/feedback.controller.js
+++ routes/feedback/feedback.controller.js
@@ -1,11 +1,14 @@
 module.exports = (app, route) => {
   route.draw(app)
     .post((req, res) => {
+      const date = new Date();
+
       const feedback = {
         "problems": req.body.problems,
         "session": req.locals.session.id,
         "version": process.env.GITHUB_SHA,
         "url": req.headers.referer || req.headers.referrer,
+        "date": date.toISOString(),
       }
       console.log(JSON.stringify({ "feedback": feedback }));
 
