a2628dc777ee43b319f3ff950e23ded405aab746
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-21 16:21:55 -0400

    Adding more stuff for cypress

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -1,3 +1,16 @@
+describe('Result Page Only tests', () => { 
+  it('should display an error when navigating directly to results page', () => { 
+    cy.visit('/en/results')
+    cy.get('[data-cy=missed-questions]')
+    cy.reportA11y()
+  })
+
+  it('should display GST Benefit no matter what', () => { 
+    cy.visit('/en/results')
+    cy.get('#gst_credit')
+  })
+})
+
 describe('Accessibility checks', () => {
   beforeEach(() => {
     cy.visit('/')
@@ -9,4 +22,32 @@ describe('Accessibility checks', () => {
     cy.get('[data-cy=next]').click()
     cy.reportA11y()
   });
+
+  it('EI Regular CERB, Mortgage, Student Loans, CCB', () => { 
+    cy.reportA11y()
+    cy.get('[data-cy=start]').click()
+    cy.get('#lost_joblost-all-income').click()
+    cy.get('[data-cy=next]').click()
+    cy.reportA11y()
+    cy.get('#no_incomelost-job-employer-closed').click()
+    cy.get('[data-cy=next]').click()
+    cy.reportA11y()
+    cy.get('#mortgage_paymentsyes-mortgage').click()
+    cy.get('[data-cy=next]').click()
+    cy.reportA11y()
+    cy.get('#ccbyes').click()
+    cy.get('[data-cy=next]').click()
+    cy.reportA11y()
+    cy.get('#student_debtyes').click()
+    cy.get('[data-cy=next]').click()
+    cy.reportA11y()
+    cy.get('[data-cy=benefit-list]')
+      .children().should('have.length', '4')
+    cy.get('#ei_regular_cerb')
+    cy.get('#mortgage_deferral')
+    cy.get('#student_loan')
+    cy.get('#ccb_payment')
+
+  })
+
 });
