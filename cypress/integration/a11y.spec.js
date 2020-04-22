a237f150c9a1d9a9383607f5497c2fc278e8afd6
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-04-22 10:22:58 -0400

    Adding tests for Error Pages
    Ensure all question pages have 1 error only
    Add some Cypress data attributes to make this easier to test

--- cypress/integration/a11y.spec.js
+++ cypress/integration/a11y.spec.js
@@ -1,11 +1,61 @@
-describe('Result Page Only tests', () => { 
-  it('should display an error when navigating directly to results page', () => { 
+function testError(route, numberOfExpectedErrors) {
+  const numErrors = numberOfExpectedErrors || 1
+  cy.visit(`en${route}`)
+  cy.get('[data-cy=next]').click()
+  cy.get('[data-cy=errors]').children().should('have.length', numErrors)
+  cy.reportA11y()
+}
+
+describe('Error Pages', () => {
+  it('Lost Job', () => {
+    testError('/lost-job')
+  })
+
+  it('No Income', () => {
+    testError('/your-situation/no-income')
+  })
+
+  it('Some Income', () => {
+    testError('/your-situation/some-income')
+  })
+
+  it('Unchanged Income', () => {
+    testError('/your-situation/unchanged-income')
+  })
+
+  it('Mortgage Payments', () => {
+    testError('/mortgage-payments')
+  })
+
+  it('CCB', () => {
+    testError('/CCB')
+  })
+
+  it('Student Debt', () => {
+    testError('/student-debt')
+  })
+
+  it('RRIF', () => {
+    testError('/RRIF')
+  })
+
+  it('Gross Income', () => {
+    testError('/gross-income')
+  })
+
+  it('Reduced Income', () => {
+    testError('/reduced-income')
+  })
+})
+
+describe('Result Page Only tests', () => {
+  it('should display an error when navigating directly to results page', () => {
     cy.visit('/en/results')
     cy.get('[data-cy=missed-questions]')
     cy.reportA11y()
   })
 
-  it('should display GST Benefit no matter what', () => { 
+  it('should display GST Benefit no matter what', () => {
     cy.visit('/en/results')
     cy.get('#gst_credit')
   })
@@ -14,16 +64,9 @@ describe('Result Page Only tests', () => {
 describe('Accessibility checks', () => {
   beforeEach(() => {
     cy.visit('/')
-  });
-  it('Navigate to first error page', () => {
-    cy.reportA11y()
-    cy.get('[data-cy=start]').click()
-    cy.reportA11y()
-    cy.get('[data-cy=next]').click()
-    cy.reportA11y()
-  });
+  })
 
-  it('EI Regular CERB, Mortgage, Student Loans, CCB', () => { 
+  it('EI Regular CERB, Mortgage, Student Loans, CCB', () => {
     cy.reportA11y()
     cy.get('[data-cy=start]').click()
     cy.get('#lost_joblost-all-income').click()
@@ -41,13 +84,10 @@ describe('Accessibility checks', () => {
     cy.get('#student_debtyes').click()
     cy.get('[data-cy=next]').click()
     cy.reportA11y()
-    cy.get('[data-cy=benefit-list]')
-      .children().should('have.length', '4')
+    cy.get('[data-cy=benefit-list]').children().should('have.length', '4')
     cy.get('#ei_regular_cerb')
     cy.get('#mortgage_deferral')
     cy.get('#student_loan')
     cy.get('#ccb_payment')
-
   })
-
-});
+})
