b62db323014a370f34efeb43c9526bb1f3128aef
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-05-06 08:59:35 -0400

    Adding a caveat to the encoding

--- routes/prepare/prepare.controller.js
+++ routes/prepare/prepare.controller.js
@@ -5,6 +5,8 @@ module.exports = (app, route) => {
     .get((req, res) => {
       // serialize the data object and throw it up on url
       const data = getSessionData(req)
+      // Please note this is purely for obfuscating the language and is not meant to secure
+      // if this scales up we will need to look at a way to represent the values in a bilingual way
       const encoded = { q: Buffer.from(JSON.stringify(data)).toString('base64')};
       const benefitsParam = new URLSearchParams(encoded);
 
