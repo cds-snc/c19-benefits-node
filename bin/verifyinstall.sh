a67bb42967945c0023c44ad23c0188a33be475a8
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-05-07 10:16:52 -0400

    replacing echo with printf

--- bin/verifyinstall.sh
+++ bin/verifyinstall.sh
@@ -9,21 +9,21 @@ checkSite() {
     site_sha=$(getSha "$1" )
 
     if [ "$3" = "$site_sha" ];  then
-        echo "✅ $1 verified"
+        printf "✅ $1 verified\n"
     else 
-        echo "🛑 $1 sha's don't match"
-        echo "$site_sha !== $3"
+        printf "🛑 $1 sha's don't match\n"
+        printf "$site_sha !== $3\n"
     fi
 
 }
 
-echo "\\nVerifying Development Deployment"
+printf "\nVerifying Development Deployment\n"
 development=$(git rev-parse origin/master)
-echo "latest commit in Development $development"
-checkSite https://cv19benefits-appservice-dev.azurewebsites.net/en/start against "$development"
+printf "latest commit in Development $development\n"
+checkSite https://cv19benefits-appservice-dev.azurewebsites.net/en/start against "$development\n"
 
-echo "\\nVerifying Production Deployment"
+printf "Verifying Production Deployment\n"
 staging=$(git rev-parse origin/staging)
-echo "latest commit in Staging $staging"
+printf "latest commit in Staging $staging\n"
 checkSite https://covid-benefits.alpha.canada.ca/en/start against "$staging"
 checkSite https://covid-prestations.alpha.canada.ca/fr/debut against "$staging"
