2291f2c907d1db6071cd6700baa6ab94c5d648a5
Kate Drwecka <katedrwecka@gmail.com>
2020-05-12 15:47:46 -0400

    add high school grad option

--- routes/question-your-situation-unchanged-income/schema.js
+++ routes/question-your-situation-unchanged-income/schema.js
@@ -4,7 +4,7 @@ const Schema = {
   unchanged_income: {
     isIn: {
       errorMessage: 'errors.multipleChoiceGeneric',
-      options: [['wfh', 'paid-leave', 'retired', 'student_2019_20']],
+      options: [['wfh', 'paid-leave', 'retired', 'student_2019_20', 'high_school_grad']],
     },
   },
 }
