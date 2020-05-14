44e0fed5379ba7972312bd186d79de44783f2a24
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-05-14 16:12:48 -0400

    Splitting

--- terraform/azure/app-service-docker.tf
+++ terraform/azure/app-service-docker.tf
@@ -14,6 +14,22 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     "project-code" = "esdc-covid-19-cds"
   }
 }
+resource "azurerm_app_service_plan" "asp_non_prod" {
+  name                = "${var.name}-non-prod"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  kind                = "Linux"
+  reserved            = true
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+  tags = {
+    "project-code" = "esdc-covid-19-cds"
+  }
+}
 
 resource "azurerm_app_service" "app_service" {
   name                = "${var.name}-appservice"
@@ -32,11 +48,13 @@ resource "azurerm_app_service" "app_service" {
   }
 
   app_settings = {
+    "ADOBE_ENV"                       = "production"
     "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.covid-benefit.instrumentation_key
     "APP_SERVICE"                     = "true"
     "AIRTABLE_API_KEY"                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.key_vault.vault_uri}secrets/${azurerm_key_vault_secret.airtable_api_key.name}/${azurerm_key_vault_secret.airtable_api_key.version})"
     "AIRTABLE_BASE_ID"                = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault.key_vault.vault_uri}secrets/${azurerm_key_vault_secret.airtable_base_id.name}/${azurerm_key_vault_secret.airtable_base_id.version})"
-    "DOCKER_ENABLE_CI"                = "true"
+    "CDN_PREFIX"                      = "//cv19-benefits-cdn.azureedge.net"
+    "COOKIE_SECRET"                   = "lhnhtvsnaavmlxjrqct"
     "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.container_registry.login_server}"
     "DOCKER_REGISTRY_SERVER_USERNAME" = azurerm_container_registry.container_registry.admin_username
     #"DOCKER_REGISTRY_SERVER_PASSWORD" = azurerm_container_registry.container_registry.admin_password
@@ -59,7 +77,7 @@ resource "azurerm_app_service_slot" "staging" {
   app_service_name    = azurerm_app_service.app_service.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_app_service_plan.asp_non_prod.id
 
   site_config {
     #linux_fx_version = "DOCKER|${azurerm_container_registry.container_registry.login_server}/${var.docker_image}:staging"
@@ -72,7 +90,7 @@ resource "azurerm_app_service_slot" "staging" {
   }
 
   app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.covid-benefit.instrumentation_key
+    "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.non_prod.instrumentation_key
     "APP_SERVICE"                     = "true"
     "DOCKER_ENABLE_CI"                = "true"
     "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.container_registry.login_server}"
@@ -98,7 +116,7 @@ resource "azurerm_app_service_slot" "dev" {
   app_service_name    = azurerm_app_service.app_service.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
+  app_service_plan_id = azurerm_app_service_plan.asp_non_prod.id
 
   site_config {
     #linux_fx_version = "DOCKER|${azurerm_container_registry.container_registry.login_server}/${var.docker_image}:staging"
@@ -111,7 +129,7 @@ resource "azurerm_app_service_slot" "dev" {
   }
 
   app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.covid-benefit.instrumentation_key
+    "APPINSIGHTS_INSTRUMENTATIONKEY"  = azurerm_application_insights.non_prod.instrumentation_key
     "APP_SERVICE"                     = "true"
     "DOCKER_ENABLE_CI"                = "true"
     "DOCKER_REGISTRY_SERVER_URL"      = "https://${azurerm_container_registry.container_registry.login_server}"
