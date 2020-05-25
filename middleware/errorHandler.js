e916716260d7ebe7aa3b5aaa514020b7f3491a7a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-05-25 10:15:41 -0400

    move logging to controller so all thrown errors are caught

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,18 +1,8 @@
-const { logger } = require('../config/winston.config');
-
 const errorHandler = (appInsights) => (err, req, res, next) => {
   // istanbul ignore next
   if (process.env.APPINSIGHTS_INSTRUMENTATIONKEY) {
     appInsights.trackException({ exception: err })
   }
-
-  logger.error(JSON.stringify({
-    'status': err.status,
-    'message': err.message,
-    'url': req.originalUrl,
-    'method': req.method,
-    'version': process.env.GITHUB_SHA,
-  }))
 
   res.locals.err = err
   next(err)
--- routes/global/global.controller.js
+++ routes/global/global.controller.js
@@ -1,4 +1,5 @@
 const { simpleRoute } = require('../../utils/route.helpers')
+const { logger } = require('../../config/winston.config');
 
 module.exports = (app, table) => {
   app.get('/test-500', (req, res) => {
@@ -30,6 +31,14 @@ module.exports = (app, table) => {
   // 500 error
   app.use(function (err, req, res, next) {
     res.status(err.status || 500)
+
+    logger.error(JSON.stringify({
+      'status': err.status,
+      'message': err.message,
+      'url': req.originalUrl,
+      'method': req.method,
+      'version': process.env.GITHUB_SHA,
+    }))
 
     res.locals.simpleRoute = (name, locale) => simpleRoute(name, locale)
     res.locals.hideBackButton = true
