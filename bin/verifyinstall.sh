28bd900ca4f690c02e129b40153623ac2d69f501
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-05-27 15:51:25 -0400

    Modify verification script (#503)
    * Modifying the verification script
    
    * Adding shell format to enforce formatting standards
    
    * Fixing script
    
    * Removing SHFMT for now

--- bin/verifyinstall.sh
+++ bin/verifyinstall.sh
@@ -1,36 +1,44 @@
 #!/bin/sh
 
-getSha() { 
-    curl "$1" -s | grep 'github-sha' |  awk -F'"' '{ print $4 } '
+getValue() {
+  curl "$1" -s | grep "$2" | awk -F'"' '{ print $4 } '
 }
 
-checkSite() { 
+getSha() {
+  getValue "$1" "github-sha"
+}
 
-    site_sha=$(getSha "$1" )
+getVersion() {
+  getValue "$1" "version"
+}
 
-    if [ "$3" = "$site_sha" ];  then
-        printf "✅ %s verified\n" "$1"
-    else 
-        printf "🛑 %s sha's don't match\n" "$1"
-        printf "$site_sha !== %s\n" "$3"
-    fi
+checkSiteSha() {
 
+  site_sha=$(getSha "$1")
+  sha=$(git rev-parse origin/"$3")
+
+  if [ "$sha" = "$site_sha" ]; then
+    printf "✅ %s matches sha in origin/%s \n" "$1" "$3"
+  else
+    printf "🛑 %s sha's don't match\n" "$1"
+    printf "$site_sha !== %s\n" "$sha"
+  fi
+
+}
+
+validateSite() {
+  printf "%s is deployed to %s\n" "$(getVersion "$1")" "$1"
 }
 
 printf "Running git fetch\n"
 git fetch
 
+printf "\nDevelopment Environment\n"
+checkSiteSha https://cv19benefits-appservice-dev.azurewebsites.net/en/start against master
+validateSite https://cv19benefits-appservice-dev.azurewebsites.net/en/start againstBranch master
+printf "\nStaging Deployment\n"
+validateSite https://cv19benefits-appservice-staging.azurewebsites.net/en/start againstBranch staging
 
-printf "\nVerifying Development Deployment\n"
-development=$(git rev-parse origin/master)
-printf "latest commit in Development %s \n" "$development"
-checkSite https://cv19benefits-appservice-dev.azurewebsites.net/en/start against "$development"
-
-printf "\nVerifying Staging Environment"
-staging=$(git rev-parse origin/staging)
-printf "latest commit in Staging %s\n" "$staging"
-checkSite https://cv19benefits-appservice-staging.azurewebsites.net/en/start against "$staging"
-
-printf "\nVerifying Production Deployment\n"
-checkSite https://covid-benefits.alpha.canada.ca/en/start against "$staging"
-checkSite https://covid-prestations.alpha.canada.ca/fr/debut against "$staging"
+printf "\nProduction Deployment\n"
+validateSite https://covid-benefits.alpha.canada.ca/en/start againstBranch staging
+validateSite https://covid-prestations.alpha.canada.ca/fr/debut againstBranch staging
