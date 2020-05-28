facdfbe42d4fde97a1270aff9be1d5ec028abb56
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-05-28 12:35:26 -0400

    check existence of elements first

--- assets/js/results-accordion.js
+++ assets/js/results-accordion.js
@@ -6,23 +6,28 @@ details.forEach((targetDetail) => {
 });
 
 const container = document.querySelector('.benefits--unavailable .benefits__list');
-container.classList.add('hidden');
 
-const expando = document.getElementById('unavailableBenefitsButton');
-expando.classList.remove('hidden');
+if (container) {
+  container.classList.add('hidden');
 
-expando.addEventListener("click", () => {
-  if (expando.getAttribute('aria-expanded') === 'true') {
-    container.classList.add('hidden')
-    container.setAttribute('aria-hidden', true)
-    expando.setAttribute('aria-expanded', false)
-    expando.querySelector('div.closed').classList.remove('hidden')
-    expando.querySelector('div.open').classList.add('hidden')
-  } else {
-    container.classList.remove('hidden')
-    container.setAttribute('aria-hidden', false)
-    expando.setAttribute('aria-expanded', true)
-    expando.querySelector('div.closed').classList.add('hidden')
-    expando.querySelector('div.open').classList.remove('hidden')
+  const expando = document.getElementById('unavailableBenefitsButton');
+  if (expando) {
+    expando.classList.remove('hidden');
+
+    expando.addEventListener("click", () => {
+      if (expando.getAttribute('aria-expanded') === 'true') {
+        container.classList.add('hidden')
+        container.setAttribute('aria-hidden', true)
+        expando.setAttribute('aria-expanded', false)
+        expando.querySelector('div.closed').classList.remove('hidden')
+        expando.querySelector('div.open').classList.add('hidden')
+      } else {
+        container.classList.remove('hidden')
+        container.setAttribute('aria-hidden', false)
+        expando.setAttribute('aria-expanded', true)
+        expando.querySelector('div.closed').classList.add('hidden')
+        expando.querySelector('div.open').classList.remove('hidden')
+      }
+    });
   }
-});
+}
