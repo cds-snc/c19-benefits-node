4a4a68cf8ef77d7fa45432b991f8d8e151d97696
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2020-05-29 14:46:32 -0400

    Add warning if cookies are disabled (#509)
    * add warning if cookies are disabled
    
    * move text to locale strings
    
    * update changelog

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ require('details-polyfill')
 require('./feedback')
 require('./back-button')
 require('./button-link')
+require('./check-cookies')
 
 const jQuery = require('jquery')
 window.$ = jQuery
