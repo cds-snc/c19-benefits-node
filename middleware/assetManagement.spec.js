89f050437ead25b36ece3694165a526e91a659e9
var-kyle <kyle.meelker@gmail.com>
2020-06-04 11:02:23 -0400

    added unit tests to check static asset verion

---
+++ middleware/assetManagement.spec.js
@@ -0,0 +1,16 @@
+const request = require('supertest')
+const app = require('../app.js')
+
+test('CSS is appended with current app version', async () => {
+  const route = app.routes.get('start')
+  const response = await request(app).get(route.path.en)
+  
+  expect(response.text).toContain('/dist/css/styles.css?' + process.env.TAG_VERSION)
+})
+
+test('app.js is appended with current app version', async () => {
+  const route = app.routes.get('start')
+  const response = await request(app).get(route.path.en)
+  
+  expect(response.text).toContain('/dist/js/app.js?' + process.env.TAG_VERSION)
+})
