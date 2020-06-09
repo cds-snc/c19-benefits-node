c6832fa5a2aec4efd310f654c77f0e6d1c5f556c
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-06-09 13:46:26 -0400

    Fix issue where selected value doesn't display

--- routes/question-province/question-province.controller.js
+++ routes/question-province/question-province.controller.js
@@ -4,12 +4,21 @@ const { Schema } = require('./schema.js')
 module.exports = (app, route) => {
   const name = route.name
 
-  route.draw(app)
+  route
+    .draw(app)
     .get((req, res) => {
-      res.render(name, routeUtils.getViewData(req, {
-        title: res.__('province.title'),
-      }))
-      
+      if (req.session.formdata === null || req.session.formdata === undefined) {
+        req.session.province = ''
+      } else {
+        req.session.province = req.session.formdata.province
+      }
+
+      res.render(
+        name,
+        routeUtils.getViewData(req, {
+          title: res.__('province.title'),
+        }),
+      )
     })
     .post(route.applySchema(Schema), route.doRedirect())
 }
