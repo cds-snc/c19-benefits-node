c23b736d9d8abe2c81e57f49e28d203ecdbe8af5
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-06-10 18:18:50 -0400

    Remove cypress plugin stuff

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,7 +15,6 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-import 'cypress-html-validate/dist/commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
