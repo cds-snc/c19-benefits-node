856fdf775d7ae817def9347baaf75cc5f7aeefd3
Eric Dube <ericdube88@gmail.com>
2020-06-10 14:54:02 -0400

    Moved create namespace out of context

--- helmfile/context-dev.sh
+++ helmfile/context-dev.sh
@@ -1,7 +1,5 @@
-ENVIRONMENT=$1
 az account set --subscription "MTS"
 az aks get-credentials --resource-group DTS-Dev --name DTS-Dev-K8S
 export RESOURCE_GROUP="DTS-Dev"
 export AKS_NAME="DTS-Dev-K8S"
-kubectl create namespace benefits-$ENVIRONMENT
 #source ./sub-scripts/set-env-variables.sh
